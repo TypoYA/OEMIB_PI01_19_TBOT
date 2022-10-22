3e7f5648fb6cc959fa836b91137f7a06629458b5
EU0kz <ketustinova4@yandex.ru>
2022-10-22 15:40:44 +0300

    commit

--- src/app.py
+++ src/app.py
@@ -19,10 +19,20 @@ def gen_markup():
 @bot.message_handler(commands=BOT_FUNCTIONS['commits'].commands)
 def get_commits(message):
     githubtoken = os.environ["GITHUBTOKEN"]
-    url = "https://api.github.com/repos/IHVH/OEMIB_PI01_19_TBOT_commits"
-    response = requests.get(url)
-    bot.send_message(text=f'privet', chat_id= message.chat.id)
-    bot.send_message(text=f'{response.text}', chat_id= message.chat.id)
+    hs = {"Accept":"application/vnd.github+json", "Autorization": f'Bearer {githubtoken}'}
+    url = "https://api.github.com/repos/IHVH/OEMIB_PI01_19_TBOT/commits"
+    response = requests.get(url, headers=hs)
+    if(response):
+        commits = response.json()
+        for cmt in commits:
+            msg = cmt["commit"]["message"]
+            url = cmt["html_url"]
+            name = cmt["commit"]["committer"]["name"]
+            date = cmt["commit"]["committer"]["date"]
+            send_msg = f'{name} - {msg} - {date} - {url}'
+            bot.send_message(text=f'{send_msg}', chat_id= message.chat.id)
+    else:
+        bot.send_message(text=f'{response.status_code}', chat_id= message.chat.id)
 
 @bot.message_handler(commands=BOT_FUNCTIONS['test1'].commands)
 def send_test(message):
